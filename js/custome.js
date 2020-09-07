db2835bb97874c0bed1b13fafef627f9bf6bc370
Roopchand Avula <roopchand.qg@gmail.com>
2020-09-07 21:21:19 +0530

    initial changes from roop

--- js/custome.js
+++ js/custome.js
@@ -13,6 +13,29 @@ $(document).ready(function(){
         $(this).parent().toggleClass("menuOpen");
     });
 
-    
-    
+    // Banner Slider
+    $('.banner-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    // Mapping onclick function
+        
+    $('.mapper').on('click',function(e){
+        e.preventDefault();
+        $('.mapContent').removeClass('active');
+        $('.mapper').removeClass('active');
+        $(this).addClass('active');
+        const elem = '#'+$(this).attr('data-attribute');
+        $(elem).addClass('active');
+    });
+
+    $('.mapContent-close').on('click',function(){
+        $('.mapContent').removeClass('active');
+        $('.mapper').removeClass('active');
+    })
+   
 });
